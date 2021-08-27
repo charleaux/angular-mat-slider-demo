1acb6c25ae605399eebe23f9830fa358ea2d725b
charleaux <ctorry@gmail.com>
2021-08-27 10:20:54 -0400

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
+  isVisible = true;
+  minValue = 0;
+  maxValue = 100;
+  stepValue = 5;
   name = 'Angular ' + VERSION.major;
 }
